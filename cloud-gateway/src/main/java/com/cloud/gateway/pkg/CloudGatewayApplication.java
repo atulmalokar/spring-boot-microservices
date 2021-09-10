3ba67f65ba5e76c09888e06ec36b9a445cdd32fe
atulmalokar <35562370+atulmalokar@users.noreply.github.com>
2021-09-10 19:22:34 +0530

    added comments

--- cloud-gateway/src/main/java/com/cloud/gateway/pkg/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/cloud/gateway/pkg/CloudGatewayApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-
+//cloud gateway service where each and every request with first interact with gateway
+//and gateway is responsible to redirect that request to required service
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
--- cloud-gateway/src/main/java/com/cloud/gateway/pkg/FallBackMethodController.java
+++ cloud-gateway/src/main/java/com/cloud/gateway/pkg/FallBackMethodController.java
@@ -3,14 +3,19 @@ package com.cloud.gateway.pkg;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//implementing fallback controller when we try to access any data but service is unavailable or down
+//particular method will return error message
 @RestController
 public class FallBackMethodController {
+	
+	//fall back method for user service
 	@GetMapping("/userServiceFallBack")
     public String userServiceFallBackMethod() {
         return "User Service is taking longer than Expected." +
                 " Please try again later";
     }
 
+	//fall back method for department service
     @GetMapping("/departmentServiceFallBack")
     public String departmentServiceFallBackMethod() {
         return "Department Service is taking longer than Expected." +
