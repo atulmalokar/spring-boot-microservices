de75401698eeaf288cd8ec579f7672e7aa1500cb
atulmalokar <35562370+atulmalokar@users.noreply.github.com>
2021-09-10 19:15:31 +0530

    added comments

--- hystrix-dashboard/src/main/java/com/hystrix/dashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/hystrix/dashboard/HystrixDashboardApplication.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+
+//hystrix dashboard to monitor running services on service dashboard
+//http://localhost:9295/hystrix/stream
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrixDashboard
