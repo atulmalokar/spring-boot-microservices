3f70a517390e71e4b86d07d2fdea6ab9cbaafb65
atulmalokar <35562370+atulmalokar@users.noreply.github.com>
2021-09-10 19:11:39 +0530

    added comments

--- department-service/src/main/java/com/microservices/departmentservice/service/DepartementService.java
+++ department-service/src/main/java/com/microservices/departmentservice/service/DepartementService.java
@@ -12,11 +12,12 @@ public class DepartementService {
 	@Autowired
 	private DepartmentRepository  departmentRepository;
 
+	//implementing save department method to save department object
 	public Department saveDepartment(Department department) {
 		
 		return departmentRepository.save(department);
 	}
-
+	//implementing find department by id method to fetch data from database
 	public Department findDepartmentById(Long departmentId) {
 		
 		return departmentRepository.findById(departmentId).get();
