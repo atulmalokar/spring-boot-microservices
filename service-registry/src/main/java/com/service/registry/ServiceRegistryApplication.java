13ed78c1591126874c37519e54966b369916e0f6
atulmalokar <35562370+atulmalokar@users.noreply.github.com>
2021-09-10 19:13:20 +0530

    added comments

--- service-registry/src/main/java/com/service/registry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/service/registry/ServiceRegistryApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//service registry service is for registering all services like user service and department service
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
