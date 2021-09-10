71060c188409d924b3159a4c7c8a7a115daaff41
atulmalokar <35562370+atulmalokar@users.noreply.github.com>
2021-09-10 19:02:57 +0530

    cloud gateway commit

---
+++ cloud-gateway/src/test/java/com/cloud/gateway/pkg/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.gateway.pkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
